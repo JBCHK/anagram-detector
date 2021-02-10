331095f19b0e1190882a292e2bc883e05f608d82
Jonathan Block <blockjonathan29@gmail.com>
2021-02-10 11:45:19 -0500

    commit tree b32b6183bfca542db785272220073db9eb8822c1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :a_word
+
+    def initialize(a_word)
+        @a_word = a_word
+    end
+
+    def match(array)
+        capsule = []
+        counter = 0
+         if array.any?{|element| element.split("").sort == @a_word.split("").sort} == false
+           return capsule
+        else
+        array.each do |element|
+            if element.split("").sort == @a_word.split("").sort
+               capsule << element
+            elsif element.split("").sort != @a_word.split("").sort
+              counter += 1  
+            end
+          end
+        end
+         capsule
+    end
+    
+
+end
+
+
+                    
+                  
+
+
